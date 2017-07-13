d63b34a000e5327b4afe33203cad24caaad0496e
eldar258 <9eldik7@gmail.com>
2017-07-13 11:25:37 +0300

    4.4. Построить пирамиду в псевдографике. [#4412]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,37 +1,33 @@
 package ru.job4j.loop;
 
 /**
-*Class Board.
+*Class Paint.
 *@author edzabarov
-*@since 9.07.2017
+*@since 13.07.2017
 */
-public class Board {
+public class Paint {
 	/**
-	* paint.
-	*@param width - width
+	* piramid.
 	*@param height - height
 	*@return sb;
 	*/
-	public String paint(int width, int height) {
+	public String piramid(int height) {
 		StringBuilder sb = new StringBuilder();
 		final String line = System.getProperty("line.separator");
-		int step = 1;
+		int width = height * 2 - 1;
 		for (int i = 1; i <= height; i++) {
-			if (i % 2 == 1) {
-				step = 1;
-			} else {
-				step = 0;
-			}
+			int step = (width - i * 2 + 1) / 2;
 			for (int j = 1; j <= width; j++) {
-				if (j % 2 == step) {
-					sb.append('x');
-				} else {
+				if (j <= step || j > (step + i * 2 - 1)) {
 					sb.append(' ');
+				} else {
+					sb.append('^');
 				}
 			}
-			sb.append(line);
+			if (i != height) {
+				sb.append(line);
+			}
 		}
-		//System.out.println(sb.toString());
 		return sb.toString();
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author edzabarov
+* @version $Id$
+* @since 13.07.2017
+*/
+public class PaintTest {
+	/**
+	*Test piramid.
+	*/
+    @Test
+   public void whenPiramidWithHeightTwoThenStringWithTwoRows() {
+        Paint paint = new Paint();
+        String result = paint.piramid(2);
+        String expected = String.format(" ^ %s^^^", System.getProperty("line.separator"));
+        assertThat(result, is(expected));
+    }
+	/**
+	*Test piramid.
+	*/
+    @Test
+    public void whenPiramidWithHeightFiveThenStringWithFiveRows() {
+		Paint paint = new Paint();
+        String result = paint.piramid(5);
+        String expected = String.format("    ^    %s   ^^^   %s  ^^^^^  %s ^^^^^^^ %s^^^^^^^^^", System.getProperty("line.separator"), System.getProperty("line.separator"), System.getProperty("line.separator"), System.getProperty("line.separator"), System.getProperty("line.separator"));
+        assertThat(result, is(expected));
+    }
+	/**
+	*Test piramid.
+	*/
+    @Test
+    public void whenPiramidWithHeightOneThenStringWithOneRows() {
+		Paint paint = new Paint();
+        String result = paint.piramid(1);
+        String expected = "^";
+        assertThat(result, is(expected));
+    }
+}
