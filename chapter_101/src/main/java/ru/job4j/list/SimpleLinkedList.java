c2a418d65192303e750182bcd08a1a0e08338310
eldar258 <9eldik7@gmail.com>
2017-10-24 13:30:26 +0300

    5.3.2. Создать контейнер на базе связанного списка  [#159]

---
+++ chapter_101/src/main/java/ru/job4j/list/SimpleLinkedList.java
@@ -0,0 +1,110 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class ru.job4j.list.
+ *
+ * @author edzabarov
+ * @since 24.10.2017
+ * @param <E> - Generic.
+ */
+public class SimpleLinkedList<E> implements Iterable<E> {
+    /**
+     * head of list.
+     */
+    private Entry<E> header = new Entry<E>(null, null, null);
+    /**
+     * number of elements in list.
+     */
+    private int size = 0;
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Entry<E> head = header;
+
+            @Override
+            public boolean hasNext() {
+                return head != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) throw new NoSuchElementException();
+                E result = head.element;
+                head = head.next;
+                return result;
+            }
+        };
+    }
+
+    /**
+     * add element.
+     * @param e -
+     * @param entry -
+     */
+    private void add(E e, Entry<E> entry) {
+        Entry<E> newEntry = new Entry<E>(e, entry, entry.prev);
+        newEntry.prev.next = newEntry;
+        newEntry.next.prev = newEntry;
+        size++;
+    }
+
+    /**
+     * get element.
+     * @param index -
+     * @return -
+     */
+    private E get(int index) {
+        return entry(index).element;
+    }
+
+    /**
+     * get link.
+     * @param index -
+     * @return -
+     */
+    private Entry<E> entry(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Entry<E> e = header;
+        for (int i = 0; i <= index; i++) {
+            e = e.next;
+        }
+        return e;
+    }
+
+    /**
+     * class chain.
+     * @param <E>
+     */
+    private static class Entry<E> {
+        /**
+         * element.
+         */
+        private E element;
+        /**
+         * next link.
+         */
+        private Entry<E> next;
+        /**
+         * prevision link.
+         */
+        private Entry<E> prev;
+
+        /**
+         * constructor.
+         * @param element -
+         * @param next -
+         * @param prev -
+         */
+        private Entry(E element, Entry<E> next, Entry<E> prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
