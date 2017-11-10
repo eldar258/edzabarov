87a6ac911a8fd5b11e57cd2cda671122217ab177
eldar258 <9eldik7@gmail.com>
2017-11-10 12:45:07 +0300

    2. Не перекрывать equals hashCode [#1005]

---
+++ chapter_101/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.map;
+
+import org.junit.Test;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * Class ru.job4j.map.
+ *
+ * @author edzabarov
+ * @since 10.11.2017
+ */
+public class UserTest {
+    @Test
+    public void whenUserPutInMapThenPrintMap() {
+        User first = new User("Lacky", 0, new GregorianCalendar(2000, 1, 1));
+        User second = new User("Lacky", 0, new GregorianCalendar(2000, 1, 1));
+        Map<User, String> map = new HashMap<>();
+        map.put(first, "one");
+        map.put(second, "two");
+        System.out.println(first.equals(second) + " " + map);
+    }
+}
