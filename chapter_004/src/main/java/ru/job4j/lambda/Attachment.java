66dd45fc78bb8bbb102b6195f8aadc3c8ed1ec1a
Eldar <9eldik7@gmail.com>
2020-10-31 17:25:26 +0300

    initial commit

--- chapter_004/src/main/java/ru/job4j/lambda/Attachment.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Attachment.java
@@ -1,2 +1,24 @@
-package ru.job4j.lambda;public class Attachment {
+package ru.job4j.lambda;
+
+public class Attachment {
+    private String name;
+    private int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{name='%s', size=%d}",
+                name, size);
+    }
 }
