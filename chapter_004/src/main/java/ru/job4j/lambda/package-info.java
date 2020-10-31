af34dd790e6ba47b083d1ff7ff43c5e36103dc06
Eldar <9eldik7@gmail.com>
2020-10-31 17:15:45 +0300

    initial commit

---
+++ chapter_004/src/main/java/ru/job4j/lambda/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Class ru.ru.job4j.lambda.
+ *
+ * @author edzabarov
+ * @since 06.09.2017
+ */
+package ru.job4j.lambda;
---
+++ chapter_004/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Class ru.ru.job4j.
+ *
+ * @author edzabarov
+ * @since 31.08.2017
+ */
+package ru.job4j;
