88ec94985ddf0af21eb715be49212b724e7b420b
Eldar <9eldik7@gmail.com>
2020-11-06 11:51:36 +0300

    Correction [#238813]

--- new_chapter_105/src/main/java/ru/job4j/tdd/MaxMin.java
+++ new_chapter_105/src/main/java/ru/job4j/tdd/MaxMin.java
@@ -5,22 +5,23 @@ import java.util.List;
 
 public class MaxMin {
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return getMaxOrMin(value, comparator, 1);
+        return getMaxOrMin(value, comparator);
     }
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return getMaxOrMin(value, comparator, -1);
+        return getMaxOrMin(value, comparator.reversed());
     }
 
-    private <T> T getMaxOrMin(List<T> value, Comparator<T> comparator, int maxOrMin) {
-        int maxIndex = value.size() - 1;
-        return maxIndex == 0 ? value.get(0)
-                : calculate(maxOrMin, value, comparator, 0, maxIndex / 2, maxIndex);
-    }
-    private <T> T calculate(int maxOrMin, List<T> value, Comparator<T> comparator, int leftStart, int rightStart, int finish) {
-        T el1 = leftStart == rightStart ? value.get(leftStart)
-                : calculate(maxOrMin, value, comparator, leftStart, (leftStart + rightStart) / 2, rightStart);
-        T el2 = rightStart + 1 == finish ? value.get(finish)
-                : calculate(maxOrMin, value, comparator,rightStart + 1, (finish + rightStart) / 2, finish);
-        return maxOrMin * comparator.compare(el1, el2) > 0 ? el1 : el2;
+    private <T> T getMaxOrMin(List<T> value, Comparator<T> comparator) {
+        T result;
+        int length = value.size();
+        if (value.size() == 0) result = null;
+        else {
+            result = value.get(0);
+            for (int i = 1; i < length; i++) {
+                T el = value.get(i);
+                result = comparator.compare(result, el) > 0 ? result : el;
+            }
+        }
+        return result;
     }
 }
